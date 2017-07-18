93d60219b65f88622ed8e6c7fe1b91710d287a84
Giuseppe Lobraico <g.lobraico@gmail.com>
2017-07-18 01:48:21 +0100

    Raise check interval to 15 minutes

--- constants.go
+++ constants.go
@@ -4,7 +4,7 @@ import "time"
 
 // Change at will.
 const minApprovals int = 2
-const checkInterval time.Duration = 30000
+const checkInterval time.Duration = 450000
 const notificationChannel string = "general"
 
 // TODO: edit this var at runtime
