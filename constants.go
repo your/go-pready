82baf89ba21f999ccb76eab0cd798cfc75eff3d9
Giuseppe Lobraico <g.lobraico@gmail.com>
2017-07-18 01:47:38 +0100

    Add slack bot and route notifications to a channel

--- constants.go
+++ constants.go
@@ -5,6 +5,7 @@ import "time"
 // Change at will.
 const minApprovals int = 2
 const checkInterval time.Duration = 30000
+const notificationChannel string = "general"
 
-// TODO: make this var at runtime
+// TODO: edit this var at runtime
 var repositories = [...]string{"_repo"}
